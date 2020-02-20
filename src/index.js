37ab47916cb433b82f69069be92462bb714d27fe
mamaNikol <49046890+mamaNikol@users.noreply.github.com>
2020-02-20 09:10:12 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = function min(array) {
+return !array || !(array.length) ? 0 :
+        array.reduce((accum, value) =>
+            Math.min(accum, value)
+        );
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return !array || !(array.length) ? 0 :
+        array.reduce((accum, value) =>
+            Math.max(accum, value)
+        );
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return !array || !(array.length) ? 0 :
+    array.reduce((accum, value) => (accum+value) / array.length;
+);
+};
